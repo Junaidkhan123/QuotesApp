62f5db1699fa092b99434c5986c36a5a259ee33a
mac <Junaid@Junaid.local>
2017-06-03 22:46:40 -0700

    Initial Commit

---
+++ QoutesApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  QoutesApp
+//
+//  Created by Junaid Khan on 03/06/2017.
+//  Copyright © 2017 mac. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
